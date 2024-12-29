9cfe6fc39d33e967df960c8de80fd52abaf8f2f6
Christian Frei <christian.frei.01@googlemail.com>
2024-12-29 23:56:10 +0100

    Start working on login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginSignupComponent } from './features/login-signup/login-signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginSignupComponent }
+];
---
+++ src/app/features/login-signup/login-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+
+@Component({
+  selector: 'app-login-signup',
+  imports: [LoginComponent],
+  templateUrl: './login-signup.component.html',
+  styleUrl: './login-signup.component.scss'
+})
+export class LoginSignupComponent {
+
+}
