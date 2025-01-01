fe2fc4f51a9d7b4b86b6f7003c7fc427e073f8dc
Christian Frei <christian.frei.01@googlemail.com>
2025-01-02 00:06:32 +0100

    Add signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginSignupComponent } from './features/login-signup/login-signup.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginSignupComponent }
+    { path: '', component: LoginSignupComponent },
+    { path: 'login', component: LoginSignupComponent },
+    { path: 'signup', component: LoginSignupComponent },
+    { path: 'privacy-policy', component: LoginSignupComponent }
 ];
--- src/app/features/login-signup/login-signup.component.ts
+++ src/app/features/login-signup/login-signup.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-signup',
-  imports: [LoginComponent],
+  imports: [LoginComponent, SignupComponent],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.scss',
   host: {
@@ -11,5 +13,9 @@ import { LoginComponent } from './login/login.component';
   }
 })
 export class LoginSignupComponent {
+  constructor(private router: Router) {}
 
+  isLogin() {
+    return this.router.url.includes('login');
+  }
 }
