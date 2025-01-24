b4712e184ad9ae80ef0b893b107b7ee0c0f51804
Christian Frei <christian.frei.01@googlemail.com>
2025-01-24 22:23:42 +0100

    Start working on components for adding tasks

--- src/app/features/main/add-task/add-task.component.ts
+++ src/app/features/main/add-task/add-task.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AddTaskFormComponent } from "../add-task-form/add-task-form.component";
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [AddTaskFormComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
