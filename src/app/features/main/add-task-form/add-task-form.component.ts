b8e9d950d0b824c0aedf469faaac25f0b4d564d5
Christian Frei <christian.frei.01@googlemail.com>
2025-04-23 21:11:03 +0200

    Implement priority button functionality in form for adding tasks

--- src/app/features/main/add-task-form/add-task-form.component.ts
+++ src/app/features/main/add-task-form/add-task-form.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-task-form',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-task-form.component.html',
   styleUrl: './add-task-form.component.scss'
 })
 export class AddTaskFormComponent {
-
+  urgency: 'urgent' | 'medium' | 'low' = 'medium';
+  
+  clickPriorityButton(buttonName: 'urgent' | 'medium' | 'low') {
+    this.urgency = buttonName;
+  }
 }
