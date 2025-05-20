f54656642ed58dbc0834a766cf0190b78af91d96
Christian Frei <christian.frei.01@googlemail.com>
2025-05-20 21:40:48 +0200

    Work on summary page

--- src/app/features/main/summary/summary.component.ts
+++ src/app/features/main/summary/summary.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../../../core/data/data.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
