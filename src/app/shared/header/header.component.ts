e6aa5d89c86571bbc03f35a894a97f80b5cfe919
Christian Frei <christian.frei.01@googlemail.com>
2025-05-31 20:38:09 +0200

    Work on dropdown menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -10,12 +11,13 @@ export class HeaderComponent {
 
   firstNameInitial: string = 'A';
   lastNameInitial: string = 'B';
+  isDropDownMenuOpen: boolean = false;
 
   logout() {
     console.log('logout');
   }
 
   showOrHideDropDownMenu() {
-    console.log('show or hide dropdown');
+    this.isDropDownMenuOpen = !this.isDropDownMenuOpen;
   }
 }
