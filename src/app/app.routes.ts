d6ffc6150841a376b8261c948a6de9617358a2e5
Christian Frei <christian.frei.01@googlemail.com>
2025-06-01 08:58:41 +0200

    Add help route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { BoardComponent } from './features/main/board/board.component';
 import { ContactsComponent } from './features/main/contacts/contacts.component';
 import { PrivacyPolicyComponent } from './features/main/privacy-policy/privacy-policy.component';
 import { LegalNoticeComponent } from './features/main/legal-notice/legal-notice.component';
+import { HelpComponent } from './features/main/help/help.component';
 
 export const routes: Routes = [
   {
@@ -29,7 +30,8 @@ export const routes: Routes = [
       { path: 'board', component: BoardComponent },
       { path: 'contacts', component: ContactsComponent },
       { path: 'privacy-policy', component: PrivacyPolicyComponent },
-      { path: 'legal-notice', component: LegalNoticeComponent }
+      { path: 'legal-notice', component: LegalNoticeComponent },
+      { path: 'help', component: HelpComponent }
     ],
   }
 ];
