dc3b158536ba989cd37b255bd56e62f0fb0c2d76
Christian Frei <christian.frei.01@googlemail.com>
2025-09-02 23:49:03 +0200

    Use workaround for certain browsers on iOS that do not support svh as it was intended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'join-angular';
+
+  isIOS = false;
+
+  constructor() {
+    this.isIOS = this.checkIOS();
+    this.setHeight(window.screen.height);
+  }
+
+  checkIOS() {
+    const platform = (navigator as any).userAgentData?.platform || navigator.platform;
+    return platform.startsWith("iP") || platform.startsWith("Mac") && navigator.maxTouchPoints > 4;
+  }
+
+  setHeight(innerHeight: number) {
+    if (this.isIOS) {
+      document.documentElement.style.setProperty('--vh100', `${innerHeight - 49}px`); 
+    }
+  }
+
+  @HostListener('window:resize', ['$event.target.screen.height'])
+  onResize(height: number) {
+    this.setHeight(height);
+  }
+  
 }
